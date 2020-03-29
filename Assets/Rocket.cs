94087598c5af3486e67b10ef1eb3be23d13d641b
James Lagattuta <jimmy.lagattuta@gmail.com>
2020-03-29 00:33:20 -0700

    rocket movement and sound

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidBody;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
@@ -20,15 +24,29 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))   //can thrust while rotating, thats why seperate if statements.
         {
-            print("Thrusting");
+            //AddRelativeForce to make ship go in one direction
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        } else
+        {
+            audioSource.Stop();
+        }
+
+        //extra boost
+        if (Input.GetKey(KeyCode.P))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
         }
 
         if (Input.GetKey(KeyCode.A))    //either A or D not both
         {
-            print("Rotating Left");
+            transform.Rotate(Vector3.forward);
         } else if (Input.GetKey(KeyCode.D))
         {
-            print("Rotating Right");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
