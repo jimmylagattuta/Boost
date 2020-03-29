a1506dda96bc64c02fb5361f69dd5d53689311bc
James Lagattuta <jimmy.lagattuta@gmail.com>
2020-03-29 15:20:44 -0700

    Rocket Ship functioning with sound effects s

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,11 +1,23 @@
-﻿using System.Collections;
+﻿//can be removed
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip winSound;
+    [SerializeField] AudioClip explosionSound;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
+
+    enum State { Alive, Dying, Transcending };
+    State state = State.Alive;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,20 +29,61 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+       if (state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
     }
 
-    private void ProcessInput()
+    void OnCollisionEnter(Collision collision)
+    {
+        if (state != State.Alive) { return; } //ignore collisions
+
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                //do nothing
+
+                break;
+            case "Finish":
+                StartSuccessSequence();
+                break;
+            default:
+                //kill player
+                StartDeathSequence();
+                break;
+        }
+    }
+    private void StartSuccessSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(winSound);
+        Invoke("LoadNextLevel", 3f); //parameterise time
+    }
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(explosionSound);
+        Invoke("LoadFirstLevel", 3f);
+    }
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1); //todo allow for more than 2 levels
+    }
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+    private void RespondToThrustInput()
     {
         if (Input.GetKey(KeyCode.Space))   //can thrust while rotating, thats why seperate if statements.
         {
-            //AddRelativeForce to make ship go in one direction
-            rigidBody.AddRelativeForce(Vector3.up);
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
-        } else
+            ApplyThrust();
+        }
+        else
         {
             audioSource.Stop();
         }
@@ -39,14 +92,30 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.P))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+   
         }
+    }
+    private void ApplyThrust()
+    {
+        //AddRelativeForce to make ship go in one direction
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+    }
+    private void RespondToRotateInput()
+    {
+        rigidBody.freezeRotation = true; //take manual control of rotation
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))    //either A or D not both
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         } else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
+        rigidBody.freezeRotation = false; //resume physics control of rotation
     }
 }
